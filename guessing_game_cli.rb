913949255ad746fb4999668bd4f0dd94f1490706
John Karrell Speck <speckworks@gmail.com>
2019-08-22 19:04:16 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,15 +1,15 @@
-def run_guessing_game
-  puts "Guess a number between 1 and 6."
-  random_number = rand(1..6)
-  user_guess = gets.chomp.strip
-  if user_guess == "exit"
-    puts "Goodbye!"
-  elsif random_number == user_guess;
-    puts "You guessed the correct number!"
-  elsif random_number != user_guess
-    puts "Sorry! The computer guessed #{number}"
-  end
-end
+# def run_guessing_game
+#   puts "Guess a number between 1 and 6."
+#   random_number = rand(1..6)
+#   user_guess = gets.chomp.strip
+#   if user_guess == "exit"
+#     puts "Goodbye!"
+#   elsif random_number == user_guess;
+#     puts "You guessed the correct number!"
+#   elsif random_number != user_guess
+#     puts "Sorry! The computer guessed #{number}"
+#   end
+# end
 
 # def run_guessing_game
 #   puts "Guess a number between 1 and 6."
