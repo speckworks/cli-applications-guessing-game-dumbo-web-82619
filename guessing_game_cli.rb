dbf9e826aa0bef0788926e076a33103ff86e8d96
John Karrell Speck <speckworks@gmail.com>
2019-08-22 18:48:07 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,7 +2,7 @@ def run_guessing_game
   number = rand(1..6)
   user_guess = gets.chomp
   if number == user_guess
-    
   elsif number != user_guess
     puts "Sorry! The computer guessed #{number}" 
+end
 end
